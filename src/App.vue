858dd8f7e987d073c1dae63f855b223a11ca0cd1
burrelle <burrelle@oregonstate.edu>
2018-02-28 13:49:54 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <form-input></form-input>
+    <task-list></task-list>
+  </div>
+</template>
+
+<script>
+import formInput from "./components/Form.vue";
+import taskList from "./components/Task.vue"
+export default {
+  name: 'app', 
+  components: { formInput, taskList }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="field">
+    <form>
+    <label class="label">Enter a Task:</label>
+    <div class="control">
+      <textarea class="textarea" placeholder="Enter your task here..." v-model="task.description" required></textarea>
+    </div>
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <label class="label">Start Date:</label>
+        <input class="input" type="date" v-model="task.startDate" required>
+      </p>
+      <p class="control is-expanded">
+        <label class="label">Expected Completion Date:</label>
+        <input class="input" type="date" v-model="task.endDate" required>
+      </p>
+    </div>
+    <div class="field is-grouped">
+      <p class="control">
+        <button class="button is-info" @click="addToList(), clearInput()">
+          Add Task
+        </button>
+      </p>
+      <p class="control">
+        <button class="button is-danger" @click="clearInput()">
+          Cancel
+        </button>
+      </p>
+    </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'form-input',
+    data() {
+      return {
+        task: {
+          description: '',
+          startDate: '',
+          endDate: '',
+          completed: false
+        },
+
+        tasks: []
+      }
+    },
+
+    methods: {
+      addToList() {
+        this.tasks.push(this.task);
+      },
+      clearInput() {
+        this.task.description = '';
+        this.task.startDate = '';
+        this.task.endDate = '';
+        this.task.completed = false;
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
